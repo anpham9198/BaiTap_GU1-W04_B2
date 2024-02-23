87d18439c0871d95c53b0cf5d10f978d416137f0
anpham9198 <158065764+anpham9198@users.noreply.github.com>
2024-02-23 10:32:00 +0700

    Initial commit

---
+++ BaiTap_GU1-W04_B2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap_GU1_W04_B2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 4, 5 };
+            Console.Write("Input: ");
+            string a = Console.ReadLine();
+
+            while (!int.TryParse(a, out int b))
+            {
+                Console.Write("Input: ");
+                a = Console.ReadLine();
+            }
+
+            bool isFound = false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == Int32.Parse(a))
+                {
+                    isFound = true;
+                }
+            if ( isFound)
+                {
+                    Console.WriteLine("found");
+                }
+                else
+                {
+                    Console.WriteLine("not found");
+                }
+
+            
+                Console.ReadKey();
+            }
+        }
+    }
+}
